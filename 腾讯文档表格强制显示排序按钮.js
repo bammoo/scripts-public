5ad02985231808664e279c321096e14db3917e67
肖建锋 <xiaojianfeng@qtrade.com.cn>
2021-11-16 10:04:22 +0800

    feat: add scripts

---
+++ 腾讯文档表格强制显示排序按钮.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name                qq dcos sheet always display sort button
+// @description         Display the sort button of sheet most front on docs.qq.com
+
+// @name:zh-CN          腾讯文档表格强制显示排序按钮
+// @description:zh-CN   腾讯文档表格强制显示排序按钮
+
+// @author              Xiaojianfeng
+// @license             MIT
+// @icon                https://pub.idqqimg.com/pc/misc/files/20200904/2eb030216d9362bbc6c0df045857b718.png
+
+// @grant               none
+// @run-at              document-idle
+// @include             https://docs.qq.com/sheet*
+
+// @date                03/15/2021
+// @modified            03/15/2021
+// @version             0.1.0
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  let timer = null;
+  function checkLink() {
+    var parent = document.querySelector(".shows-view");
+    var sort = document.getElementById("sheet-sort-button");
+    if (parent && sort) {
+      // parent.appendChild(sort);
+      parent.insertBefore(sort, parent.childNodes[0]);
+      sort.style.setProperty("display", "inline-block", "important");
+
+      clearInterval(timer);
+    }
+  }
+
+  timer = setInterval(checkLink, 100);
+})();
---
+++ 顶部显示知乎专栏文章日期.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name                顶部显示知乎专栏文章日期
+// @namespace           http://tampermonkey.net/
+// @description         try to take over the world!
+
+// @author              Xiaojianfeng
+// @license             MIT
+
+// @grant               none
+// @run-at              document-idle
+// @match               https://zhuanlan.zhihu.com/*
+
+// @version             0.2
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  var target = document.querySelector(".ContentItem-time");
+  var date1 = target.innerText;
+  target.click();
+  var date2 = target.innerText;
+  var newContent = `<span style="margin-right: 20px;">${date1}</span>`;
+  if (date2 && date2 !== date1) {
+    newContent += `<span>${date2}</span>`;
+  }
+  var insertPosition = document.querySelector(".Post-Title");
+  insertPosition.insertAdjacentHTML(
+    "afterend",
+    `<div style="margin-bottom: 15px; font-size:14px; color: #1a1a1a;">${newContent}</div>`
+  );
+})();
